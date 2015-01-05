068a54a8e5f8a19859931c66cc23ee3536b4383b
Cody Dracars <cdracars@gmail.com>
2015-01-05 17:03:13 -0600

    a little work to make it not send two

--- weekly-calendar-report.sh
+++ weekly-calendar-report.sh
@@ -5,10 +5,15 @@ tfile1=$(mktemp /tmp/newCalXXXXXX)
 tfile2=$(mktemp /tmp/newCalXXXXXX)
 tfile3=$(mktemp /tmp/newCalXXXXXX)
 to='cdracars@usao.edu, rvollmar@usao.edu, jjackson@usao.edu, karnold@usao.edu, kchambers@usao.edu'
+#to='cdracars@usao.edu'
 db='drupal'
 
 # Select new calendar entries from database
-mysql $db -t -e "SELECT node.title, node.nid FROM node node WHERE (DATE_FORMAT(ADDTIME(FROM_UNIXTIME(node.created), SEC_TO_TIME(-18000)), '%Y-%m-%d') > DATE_SUB(CURDATE(), INTERVAL 7 DAY)) AND (node.type in ('event')) ORDER BY node.created" > $tfile1
+calItems=$(mysql $db -t -e "SELECT node.title, node.nid FROM node node WHERE (DATE_FORMAT(ADDTIME(FROM_UNIXTIME(node.created), SEC_TO_TIME(-18000)), '%Y-%m-%d') > DATE_SUB(CURDATE(), INTERVAL 7 DAY)) AND (node.type in ('event')) ORDER BY node.created") 
+
+if [ -n "$calItems" ]; then
+  echo $calItems > $tfile1
+fi
 
 # filter out any tables we don't really care about
 cat $tfile1 | grep [0-9] | sed -e 's/^|.//' \
@@ -23,7 +28,7 @@ cat $tfile2 | awk -F="http" 'BEGIN { print "<h2>Summary</h2><table border=1>" }
 
 # Send an email to Cody with the summary body and details attached.
 # the -n checks to see if there is anything in the variable and only sends out non empty messages.
-if [ -n ${tfile3} ]; then
+if [ -n "$calItems" ]; then
 cat <<EOF | mutt -e "set content_type=text/html" -s "Weekly New Calendar Items (New - D7)" $to
 <html>
 <p>Good Morning Media Team!
